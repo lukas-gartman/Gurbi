d3a7cdf4ac945b6549c13e8fc4a358e79b8e419b
Rakarake <rak@rakarake.xyz>
2024-02-06 15:59:28 +0100

    React project created, test module, nix flake file

---
+++ client/src/BingChilling.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function BingChilling() {
+  return (
+    <p className="BingChilling">
+    Hi there, my name is Markiplier!
+    <a href="https://rakarake.xyz">My website 🦖</a>
+    </p>
+  );
+}
+
+export default BingChilling;
