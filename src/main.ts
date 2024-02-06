92eac5449f3e2c0507dada12d7142e2de6eea416
skofta <35774710+skofta@users.noreply.github.com>
2024-02-06 14:50:05 +0100

    lab 2 start

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import express, { Express, Request, Response } from "express";
-
-const app : Express = express();
-app.get("/",
-  async (req : Request, res : Response) => {
-    res.send("Hello World!");
-  });
-app.listen(8080);
