06e7c0d9414ef2bbe44098793d563c5bb86f2540
skofta <35774710+skofta@users.noreply.github.com>
2024-02-06 22:52:34 +0100

    Revert "small"
    This reverts commit 1b4c6124c8195bfc1fbf9bcc457ed4b3cb2beb01.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,11 @@
 
 import express from "express";
-import {UserRegistartionRouter} from "./router/registartionRouter";
-
-//import { taskRouter } from "./router/task";
+import { taskRouter } from "./router/task";
 
 export const app = express();
 
 app.use(express.json());
-
 //app.use("/task", taskRouter);
-
-app.use("/user", UserRegistartionRouter);
 
 /**
 * App Variables
