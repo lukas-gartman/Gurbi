8a66574af2a29c780cf3fce6a1ef3d5284fcbec3
Albin a12l Otterhäll <dev@a12l.xyz>
2024-02-13 16:11:44 +0100

    user: wip

--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -5,11 +5,12 @@ import { UserLogin } from "../model/dataModels";
 export const userRouter : Router = express.Router();
 
 userRouter.post( "/", async(
-        req: Request<{}, {}, {}>,
+        req: Request<{}, {}, {name: string, email: string, password: string}>,
         res: Response<string>
     ) => {
         try {
-            const newUser: User = new User("John Doe", "hello@example.com", "password123");
+            const newUser: User = new User(req.body.name, req.body.email, req.body.password);
+            res.status(200).send(newUser);
         } catch (e: any) {
             res.status(500).send(e.message);
         }
