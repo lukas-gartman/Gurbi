58387aed39ce652fb14a67c73c988504fcd99850
skofta <35774710+skofta@users.noreply.github.com>
2024-02-27 19:41:18 +0100

    app Variables for database

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,17 +9,19 @@ import { DBconnHandler } from "./db/database";
 * App Variables
 */
 const PORT : number = 8080;
-
+const USE_DATABASE : boolean = false;
+const DATABASE_URI : string = "mongodb://localhost:27017/dat076"
 /**
 * Server Activation
 */
 
 
 async function startServer(){
-    //let uri : string = "mongodb://localhost:27017/dat076"
-    //await DBconnHandler.newConn(uri)
+    if(USE_DATABASE){
+        await DBconnHandler.newConn(DATABASE_URI)
+    }
     
-    let app : Application = getApp(false)
+    let app : Application = getApp(USE_DATABASE)
 
     app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
