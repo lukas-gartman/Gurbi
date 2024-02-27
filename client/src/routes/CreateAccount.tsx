3bb46c6f093d370a370cc2e6729f5783efb059d3
lukas-gartman <gusgartlu@student.gu.se>
2024-02-27 11:28:15 -0500

    Update forms to be handled in React standard

--- client/src/routes/CreateAccount.tsx
+++ client/src/routes/CreateAccount.tsx
@@ -1,17 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../stylesheets/Form.css';
 import { Link } from 'react-router-dom';
 
 function CreateAccount() {
+    interface CreateAccountFormData {
+        fullName: string;
+        nickname: string;
+        email: string;
+        password: string;
+        repeatPassword: string;
+    };
+
+    const [formData, setFormData] = useState<CreateAccountFormData>({fullName: "", nickname: "", email: "", password: "", repeatPassword: ""});
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value});
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            await axios.post("/account/create", formData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="App">
             <main className="form-container">
                 <h1>Create account</h1>
-                <form className="form" action="/account/create" method="POST">
-                    <input type="text" name="name" placeholder="Full name" required />
-                    <input type="text" name="nickname" placeholder="Nickname (optional)" required />
-                    <input type="text" name="email" placeholder="Email" required />
-                    <input type="password" name="password"  placeholder="Password" required />
-                    <input type="password" name="password2" placeholder="Re-enter password" required />
+                <form className="form" onSubmit={handleSubmit}>
+                    <input type="text" onChange={handleChange} placeholder="Full name" required />
+                    <input type="text" onChange={handleChange} placeholder="Nickname (optional)" required />
+                    <input type="text" onChange={handleChange} placeholder="Email" required />
+                    <input type="password" onChange={handleChange} placeholder="Password" required />
+                    <input type="password" onChange={handleChange} placeholder="Re-enter password" required />
                     <input type="submit" value="Create account" />
                 </form>
                 <Link to="/">Already have an account?</Link>
--- client/src/routes/Settings.tsx
+++ client/src/routes/Settings.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../stylesheets/Settings.css';
 import '../stylesheets/Form.css';
 import { NavLink, useLoaderData, useNavigate } from 'react-router-dom';
@@ -5,6 +7,8 @@ import Header from '../components/Header';
 import { IUser } from "../../../src/server/model/dataModels";
 
 function Profile() {
+    const user = useLoaderData() as IUser;
+
     let nav = useNavigate();
     const goBack = () => {
         nav(-1);
@@ -19,22 +23,42 @@ function Profile() {
         </>
     );
 
-    const user = useLoaderData() as IUser;
+    interface UpdatePasswordFormData {
+        currentPassword: string;
+        newPassword: string;
+        repeatPassword: string;
+    };
+
+    const [formData, setFormData] = useState<UpdatePasswordFormData>({currentPassword: "", newPassword: "", repeatPassword: ""});
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value});
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            await axios.post("/profile/settings/update_password", formData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="App">
             <Header headerContent={header} />
             <main className="settings">
                 <h3>Update password</h3>
                 <div className="form-container">
-                    <form className="form" action="/profile/settings/update_password" method="POST">
-                        <input type="password" name="current_password" placeholder="Current password" required />
-                        <input type="password" name="new_password" placeholder="New password" required />
-                        <input type="password" name="repeat_new_password" placeholder="Repeat new password" required />
+                    <form className="form" onSubmit={handleSubmit}>
+                        <input type="password" onChange={handleChange} placeholder="Current password" required />
+                        <input type="password" onChange={handleChange} placeholder="New password" required />
+                        <input type="password" onChange={handleChange} placeholder="Repeat new password" required />
                         <input type="submit" value="Update password" />
                     </form>
                 </div>
-
-
             </main>
         </div>
     );
