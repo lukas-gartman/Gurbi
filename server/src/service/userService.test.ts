ef30220aae8dcf89964e5842ad2ec7bddd2ba053
Albin a12l Otterhäll <dev@a12l.xyz>
2024-03-12 22:24:31 +0100

    users: Add basic tests

---
+++ server/src/service/userService.test.ts
@@ -0,0 +1,74 @@
+import { userServiceResponse, UserLogin, IUser } from "../model/UserModels";
+import { UserService } from "./userService";
+
+test("Create new user", async () => {
+    const userService = new UserService();
+    
+    const newUser = {
+        fullName: "Kalle Anka",
+        nickname: "Kalle",
+        email: "kalle@anka.se",
+        password: "abc123",
+        repeatPassword: "abc123",
+    };
+    
+    let answer: userServiceResponses = await UserService.addUser(newUser);
+    
+    expect(answer).toStrictEqual(userServiceResponse.goRes(4));
+})
+
+test("Create new user without nickname", async () => {
+    const userService = new UserService();
+    
+    const newUser = {
+        fullName: "Kalle Anka",
+        email: "kalle@anka.se",
+        password: "abc123",
+        repeatPassword: "abc123",
+    };
+    
+    let answer: userServiceResponses = await UserService.addUser(newUser);
+    
+    expect(answer).toStrictEqual(userServiceResponse.goRes(1));
+})
+
+test("Create new user with nonmatching password", async () => {
+    const userService = new UserService();
+    
+    const newUser = {
+        fullName: "Kalle Anka",
+        nickname: "Kalle",
+        email: "kalle@anka.se",
+        password: "abc123",
+        repeatPassword: "123abc", /* Different order */
+    };
+    
+    let answer: userServiceResponses = await UserService.addUser(newUser);
+    
+    expect(answer).toStrictEqual(userServiceResponse.goRes(2));
+})
+
+test("Create multiple users with same email", async () => {
+    const userService = new UserService();
+    
+    const newUserKalle = {
+        fullName: "Kalle Anka",
+        nickname: "Kalle",
+        email: "kalle@anka.se",
+        password: "abc123",
+        repeatPassword: "abc123",
+    };
+    
+    const newUserKajsa = {
+        fullName: "Kajsa Anka",
+        nickname: "Kaja",
+        email: "kalle@anka.se",
+        password: "abc123",
+        repeatPassword: "abc123",
+    };
+    
+    await UserService.addUser(newUserKalle);
+    let answer: userServiceResponses = await UserService.addUser(newUserKajsa);
+    
+    expect(answer).toStrictEqual(userServiceResponse.goRes(3));
+})
